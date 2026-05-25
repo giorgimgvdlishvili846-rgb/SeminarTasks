1a5cf18d9a14073105c5af2eefb2ddb8f0863478
Giorgi Mgvlishvili <gmghvdlishvili@gmail.com>
2026-05-25 14:20:56 +0400

    Seminar Taks

---
+++ Task10/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Task10;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       
+    }
+}
---
+++ Task9/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Task9;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int counter = 0;
+        while (true)
+        {
+            Console.Write("Enter the number: ");
+            int number = int.Parse(Console.ReadLine());
+            if (number % 2 == 0)
+            {
+                counter++;
+            }
+            if (number == -1)
+            {
+                break;
+            }
+        }
+        Console.WriteLine($"The even numbers are: {counter}");
+    }
+}
